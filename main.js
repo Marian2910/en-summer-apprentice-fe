b421d1485bfa2df4c32c76288d79c9dd0e28463f
Marian Mutu <marian.mutu02@gmail.com>
2023-08-11 18:22:50 +0300

    finished post

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-import { kebabCase, addPurchase } from "./utils";
+import { addPurchase } from "./utils";
 import { useStyle } from "./src/components/styles.js";
 
 
-// Navigate to a specific URL
 function navigateTo(url) {
   history.pushState(null, null, url);
   renderContent(url);
 }
-// HTML templates
 function getHomePageTemplate() {
   return `
    <div id="content" >
-      <img src="./src/assets/Endava.png" alt="summer">
       <div class="events flex items-center justify-center flex-wrap">
       </div>
     </div>
@@ -90,8 +87,6 @@ const addEvents = (events) => {
     eventsDiv.innerHTML = '';
     events.forEach(event => {
       const eventElement = createEvent(event);
-      console.log('Fetched events:', eventElement);
-
       if (eventElement) {
         eventsDiv.appendChild(eventElement);
 
@@ -103,16 +98,15 @@ const addEvents = (events) => {
   const createEvent = (eventData) => {
     if (!eventData || !eventData.eventType) {
       console.error('Invalid eventData:', eventData.eventType);
-      return null; // Handle this case accordingly
+      return null;
     }
 
-    const title = kebabCase(eventData.eventType);
-    const eventElement = createEventElement(eventData, title);
+    const eventElement = createEventElement(eventData);
     return eventElement;
   };
 
-  const createEventElement = (eventData, title) => {
-    const { id, description, img, name, ticketCategories } = eventData;
+  const createEventElement = (eventData) => {
+    const ticketCategories = eventData.ticketCategories;
     const eventDiv = document.createElement('div');
     const eventWrapperClasses = useStyle('eventWrapper');
     const actionsWrapperClasses = useStyle('actionsWrapper');
@@ -123,36 +117,58 @@ const addEvents = (events) => {
     const decreaseBtnClasses = useStyle('decreaseBtn');
     const addToCartBtnClasses = useStyle('addToCartBtn');
 
-    // Set up event wrapper
+    const startDate = new Date(eventData.startDate);
+    const endDate = new Date(eventData.endDate);
+
+const formattedStartDate = new Intl.DateTimeFormat('ro-RO', { month: 'long', day: 'numeric' }).format(startDate);
+const formattedEndDate = new Intl.DateTimeFormat('ro-RO', { month: 'long', day: 'numeric' }).format(endDate);
+
+let formattedDateRange;
+if (formattedStartDate === formattedEndDate) {
+  formattedDateRange = `${formattedStartDate}`;
+} else {
+  formattedDateRange = `${formattedStartDate} - ${formattedEndDate}`;
+}
+const startingHour = startDate.getHours();
+const formattedStartingHour = startingHour < 10 ? `0${startingHour}` : startingHour;
+
+    
     eventDiv.classList.add(...eventWrapperClasses);
 
-    // Create the event content markup
      const contentMarkup = `
-      <header>
-        <h2 class="event-title text-2xl font-bold">${eventData.eventName}</h2>
-      </header>
-      <div class="content">
-      <img src="./src/assets/${eventData.image}" alt="${eventData.eventName}" class="event-image rounded object-cover" width="150" height="150">
-      <p class="description text-gray-700">${eventData.eventDescription}</p>
-      </div>
+     <div class="centered-container">
+     <header>
+       <h2 class="event-title text-2xl font-bold">${eventData.eventName}</h2>
+     </header>
+     <div class="image">
+       <img src="./src/assets/${eventData.image}" alt="${eventData.eventName}" class="event-image rounded object-cover" width="150" height="150">
+     </div>
+     <div>
+      <p class="description">${eventData.eventDescription}</p>
+    </div>
+     <div class="content">
+      <p class="date-range">${formattedDateRange}, de la ${formattedStartingHour}:00</p>
+    </div>
       `;
     eventDiv.innerHTML = contentMarkup;
 
-    // Create ticket type selection and quantity input
     const actions = document.createElement('div');
     actions.classList.add(...actionsWrapperClasses);
 
     const categoriesOptions = ticketCategories.map(
       (ticketCategory) =>
-      `<option value=${ticketCategory.ticketCategoryId.id}> ${ticketCategory.description} </option>`
+      `<option value=${ticketCategory.ticketCategoryId}> ${ticketCategory.description} </option>`
     );
 
     const ticketTypeMarkup = `
-    <h2 class="text-lg font-bold mb-2">Choose Ticket Type:</h2>
-    <select id="ticketType" name="ticketType" class="select ${title}-ticket-type border border ...">
-    ${categoriesOptions.join('\n')}
+  <div class="ticket-type-container">
+    <h2 class="text-lg">Alege categoria:</h2>
+    <select id="ticketType" name="ticketType" class="ticketType">
+      ${categoriesOptions.join('\n')}
     </select>
-    `;
+    <br>
+  </div>
+`;
 
     actions.innerHTML = ticketTypeMarkup;
 
@@ -160,28 +176,20 @@ const addEvents = (events) => {
     quantity.classList.add(...quantityClasses);
 
     const input = document.createElement('input');
-    input.classList.add(... inputClasses) ;
+    input.classList.add(...inputClasses) ;
 
     input.type = 'number';
     input.min = '0';
     input.value = '0';
 
-    input.addEventListener ('blur', () => {
+    input.addEventListener('blur', () => {
     if (!input.value) {
-    input.value = 0;
+      input.value = 0;
     }
 });
-  input.addEventListener('input', () => {
-    const currentQuantity = parseInt(input.value);
-    if(currentQuantity > 0) {
-      addToCart.disabled = false;
-    } else {
-      addToCart.disabled = true;
-    }
-  });
 
   input.addEventListener('input', () => {
-    const currentQuantity = parseInt (input.value);
+    const currentQuantity = parseInt(input.value);
     if (currentQuantity > 0) {
       addToCart.disabled = false;
     } else {
@@ -194,36 +202,34 @@ const addEvents = (events) => {
     const quantityActions = document.createElement ('div');
     quantityActions.classList.add(...quantityActionsClasses);
 
-    const increase = document.createElement ('button');
-    increase.classList.add (...increaseBtnClasses) ;
-
+    function updateQuantity(addToCart, input) {
+      const currentQuantity = parseInt(input.value);
+      if (currentQuantity > 0) {
+        addToCart.disabled = false;
+      } else {
+        addToCart.disabled = true;
+      }
+    }
+    
+    const increase = document.createElement('button');
+    increase.classList.add(...increaseBtnClasses);
     increase.innerText = '+';
     increase.addEventListener('click', () => {
       input.value = parseInt(input.value) + 1;
-      const currentQuantity = parseInt(input.value) ;
-    if (currentQuantity > 0) {
-      addToCart.disabled = false;
-    } else {
-      addToCart.disabled = true;
-    }
-  });
-
-  const decrease = document.createElement('button');
-  decrease.classList.add(...decreaseBtnClasses) ;
-  decrease.innerText = '-';
-  decrease.addEventListener('click', () => {
-  const currentValue = parseInt(input.value);
-  if (currentValue > 0) {
-    input .value = currentValue - 1;
-  }
-  const currentQuantity = parseInt(input.value) ;
-  if (currentQuantity > 0) {
-    addToCart.disabled = false;
-  } else {
-    addToCart.disabled = true;
-  }
-});
-
+      updateQuantity(addToCart, input);
+    });
+    
+    const decrease = document.createElement('button');
+    decrease.classList.add(...decreaseBtnClasses);
+    decrease.innerText = '-';
+    decrease.addEventListener('click', () => {
+      const currentValue = parseInt(input.value);
+      if (currentValue > 0) {
+        input.value = currentValue - 1;
+      }
+      updateQuantity(addToCart, input);
+    });
+    
 quantityActions.appendChild(increase);
 quantityActions.appendChild(decrease);
 
@@ -238,7 +244,9 @@ addToCart.classList.add(...addToCartBtnClasses);
 addToCart.innerText = 'Add To Cart';
 addToCart.disabled = true;
 addToCart.addEventListener('click', () => {
-  // handleAddToCart(title, id, input, addToCart); 
+  const selectedTicketCategoryId = document.getElementById('ticketType').value;
+  handleAddToCart(eventData.eventID, selectedTicketCategoryId, input, addToCart);
+
 });
 
 eventFooter.appendChild(addToCart);
@@ -247,13 +255,44 @@ eventDiv.appendChild(eventFooter) ;
 return eventDiv;
   }
 
+  const handleAddToCart = (eventID, selectedTicketCategoryId, input, addToCart) => {
+    const quantity = parseInt(input.value);
+    const ticketCategory = parseInt(selectedTicketCategoryId);
+    const event = parseInt(eventID);
+    
+    if (parseInt(quantity)) {
+      fetch('http://localhost:8080/orders/post', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          eventId: event,
+          ticketCategoryId: ticketCategory,
+          numberOfTickets: +quantity,
+        })
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          addPurchase(data);
+          console.log("Done!");
+          input.value = 0;
+          addToCart.disabled = true;
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } else {
+      console.error("quantity should be an integer", error);
+    }
+  };
 
-function renderOrdersPage(categories) {
+
+function renderOrdersPage() {
   const mainContentDiv = document.querySelector('.main-content-component');
   mainContentDiv.innerHTML = getOrdersPageTemplate();
 }
 
-// Render content based on URL
 function renderContent(url) {
   const mainContentDiv = document.querySelector('.main-content-component');
   mainContentDiv.innerHTML = '';
@@ -265,7 +304,6 @@ function renderContent(url) {
   }
 }
 
-// Call the setup functions
 setupNavigationEvents();
 setupMobileMenuEvent();
 setupPopstateEvent();
